0f82f5e7701ae3668485d4f69787dc2f3b62505b
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-09-05 21:58:22 +0300

    16 - product card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,11 @@ const Homepage = () => {
   return (
     <div className="space-y-8">
       <h2 className="h2-bold">Latest Products</h2>
-      <ProductList title="Newest Arrivals" data={sampleData.products} />
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
     </div>
   );
 };
