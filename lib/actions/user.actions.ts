b7b0db77ff356b3fdc9047a2e07f55c6f07f764b
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-09-09 15:49:10 +0300

    34 - sign in & sign out action

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { signIn, signOut } from "@/auth";
+import { signInFormSchema } from "../validators";
+
+function isRedirectError(error: unknown): boolean {
+  return error instanceof Error && error.message === "NEXT_REDIRECT";
+}
+
+// Sign in the user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    // Set user from form and validate it with Zod schema
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    await signIn("credentials", user);
+
+    return { success: true, message: "Signed in successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: "Invalid email or password" };
+  }
+}
+
+// Sign the user out
+export async function signOutUser() {
+  await signOut();
+}
