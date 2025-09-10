f22faddd0e418ca69dba237e46dea93502d1bb3b
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-09-10 22:06:16 +0300

    37 - hook up sign in form

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -5,10 +5,27 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    message: "",
+    success: false,
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -33,10 +50,12 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign In with credentials
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
 
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
